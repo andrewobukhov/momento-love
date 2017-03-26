2c1dd728a4641873c186f197c3686da25b06bc76
Andrew Obukhov <andrew.obukhov@nordigy.ru>
2017-03-26 20:25:29 +0300

    Initial Commit

---
+++ MomentoLove/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MomentoLove
+//
+//  Created by Andrew Obukhov on 26/03/2017.
+//  Copyright © 2017 Andrew Obukhov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
