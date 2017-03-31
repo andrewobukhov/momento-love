6b7f4a9f0939b91dd405027861559a2060383f69
Andrew Obukhov <andrew.obukhov@nordigy.ru>
2017-03-31 19:25:23 +0300

    Update Login Page

--- MomentoLove/SexController.swift
+++ MomentoLove/SexController.swift
@@ -14,7 +14,7 @@ class SexController: UIViewController {
     @IBAction func maleButton(_ sender: UIButton) {
         goNext(sex: Sex.male)
     }
-
+    
     @IBAction func femaleButton(_ sender: UIButton) {
         goNext(sex: Sex.female)
     }
@@ -26,7 +26,7 @@ class SexController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if let navBar = self.navigationController?.navigationBar {
             extendedLayoutIncludesOpaqueBars = true
             navBar.isTranslucent = true
@@ -35,28 +35,13 @@ class SexController: UIViewController {
             navBar.shadowImage = UIImage()
             self.navigationItem.backBarButtonItem = UIBarButtonItem(title:"", style:.plain, target:nil, action:nil)
             
-            
-            navBar.tintColor = UIColor.lightGray
+            navBar.tintColor = UIColor.red
         }
-
-        
-        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
